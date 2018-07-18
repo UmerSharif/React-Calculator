fd02b8e10b1e8aac7c1064feeb2d9d0f775a8885
umer khan <umer.kdev@gmail.com>
2018-07-18 23:39:39 +0300

    Addding button class

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class Button extends React.Component {
+    render(){
+        return(
+
+            <div className="button">
+
+            </div>
+        );
+    }
+}
+export default Button;
