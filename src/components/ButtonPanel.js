dc28120a41f48209418072ebfbf40f9194a134d3
umer khan <umer.kdev@gmail.com>
2018-07-18 23:42:06 +0300

    Addding buttonPanel class

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class ButtonPanel extends React.Component {
+    render(){
+        return(
+            <div className="ButtonPanel">
+
+            </div>
+        );
+    }
+}
+
+export default ButtonPanel;
