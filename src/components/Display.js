7751150450f0b316b34c72b193e91540317b82ae
umer khan <umer.kdev@gmail.com>
2018-07-19 19:49:59 +0300

    Importing display class

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,8 +4,10 @@ class Display extends React.Component {
     render(){
         return(
             <div className="display">
-
+                <h1>This is react calculator</h1>
             </div>
         );
     }
 }
+
+export default Display;
